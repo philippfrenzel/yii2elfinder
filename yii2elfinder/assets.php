fd949b81b642bd7222e1f2403d40de12efa6f992
Philipp Frenzel <philipp@frenzel.net>
2013-06-18 19:17:51 +0200

    fixing asset bundle

--- yii2elfinder/assets.php
+++ yii2elfinder/assets.php
@@ -4,11 +4,12 @@ return array(
 	'yii2elfinder/core' => array(
 		'sourcePath' => __DIR__ . '/assets',
 		'js' => array(
-			'js/elfinder.js',
+			'js/elfinder.min.js',
 			'js/i18n/elfinder.de.js'
 		),
 		'css' => array(
-			'css/elfinder.css',
+			'css/elfinder.min.css',
+			'css/theme.css'
 		),
 		'depends'=>array(
 			'yii/jquery',
