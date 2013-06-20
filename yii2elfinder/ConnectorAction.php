9b20fb034f33635d493fdb2ff1c25bb34c4b0bb1
Philipp Frenzel <philipp@frenzel.net>
2013-06-20 16:44:10 +0200

    adding new connector actions

--- yii2elfinder/ConnectorAction.php
+++ yii2elfinder/ConnectorAction.php
@@ -14,10 +14,36 @@
 
 namespace yii2elfinder;
 
+// error_reporting(0);
+ini_set('max_file_uploads', 50); // allow uploading up to 50 files at once
+
+// needed for case insensitive search to work, due to broken UTF-8 support in PHP
+ini_set('mbstring.internal_encoding', 'UTF-8');
+ini_set('mbstring.func_overload', 2);
+
+if (function_exists('date_default_timezone_set')) {
+	date_default_timezone_set('Europe/Berlin');
+}
+
 require_once(__DIR__ . '/php/elFinderConnector.class.php');
 require_once(__DIR__ . '/php/elFinder.class.php');
 require_once(__DIR__ . '/php/elFinderVolumeDriver.class.php');
 require_once(__DIR__ . '/php/elFinderVolumeLocalFileSystem.class.php');
+
+/**
+* Simple function to demonstrate how to control file access using "accessControl" callback.
+* This method will disable accessing files/folders starting from '.' (dot)
+*
+* @param string $attr attribute name (read|write|locked|hidden)
+* @param string $path file path relative to volume root directory started with directory separator
+* @return bool|null
+**/
+function access($attr, $path, $data, $volume) {
+	return strpos(basename($path), '.') === 0 // if file/folder begins with '.' (dot)
+	? !($attr == 'read' || $attr == 'write') // set read+write to false, other (locked+hidden) set to true
+	: null; // else elFinder decide it itself
+}
+
 
 use Yii;
 use \yii\base\Action;
