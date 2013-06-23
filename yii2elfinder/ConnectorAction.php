0f33747b92ec9d2b1b0a03fe66ccc48f215b4dda
Philipp Frenzel <philipp@frenzel.net>
2013-06-23 19:27:59 +0200

    case sensitive with names

--- yii2elfinder/ConnectorAction.php
+++ yii2elfinder/ConnectorAction.php
@@ -29,7 +29,7 @@ require_once(__DIR__ . '/php/elFinderConnector.class.php');
 require_once(__DIR__ . '/php/elFinder.class.php');
 require_once(__DIR__ . '/php/elFinderVolumeDriver.class.php');
 require_once(__DIR__ . '/php/elFinderVolumeLocalFileSystem.class.php');
-require_once(__DIR__ . '/php/elFinderVolumeDropBox.class.php');
+require_once(__DIR__ . '/php/elFinderVolumeDropbox.class.php');
 
 use Yii;
 use \yii\base\Action;
