4158500557a14e60541f9f4d195b9ebc5c50a96c
Philipp Frenzel <philipp@frenzel.net>
2013-10-02 15:35:38 +0200

    fix connector action

--- yii2elfinder/ConnectorAction.php
+++ yii2elfinder/ConnectorAction.php
@@ -31,8 +31,8 @@ require_once(__DIR__ . '/php/elFinderVolumeDriver.class.php');
 require_once(__DIR__ . '/php/elFinderVolumeLocalFileSystem.class.php');
 require_once(__DIR__ . '/php/elFinderVolumeDropbox.class.php');
 
-use Yii;
-use \yii\base\Action;
+use \Yii;
+use yii\base\Action;
 
 use elFinder;
 use elFinderConnector;
